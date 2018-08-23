bd8ed6c1d907c1cc144cb2b6753eed81be1ce3e1
jslusser <jslusser@gmail.com>
2018-08-23 11:34:36 -0400

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by James Slusser on 8/23/18.
+//  Copyright © 2018 James Slusser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
